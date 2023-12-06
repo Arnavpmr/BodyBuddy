24bedf858aa62209d67d903ada0629de1dd3c1de
Arnavpmr <arnavmar16@gmail.com>
2023-12-06 17:44:55 -0500

    Implemented pre commit hook to lint code

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,141 +1,127 @@
-//import express, express router as shown in lecture code
-import userDataHelpers from '../data/users.js';
-import helper from '../helpers.js';
-import { Router } from 'express';
+import userDataHelpers from "../data/users.js";
+import helper from "../helpers.js";
+import { Router } from "express";
 
 const router = Router();
 
-router.route('/').get(async (req, res) => {
-    return res.json({
-        error: 'YOU SHOULD NOT BE IN THIS ROUTE DUE TO MIDDLEWARE #1',
-    });
+router.route("/").get(async (req, res) => {
+  return res.json({
+    error: "YOU SHOULD NOT BE IN THIS ROUTE DUE TO MIDDLEWARE #1",
+  });
 });
 
 router
-    .route('/register')
-    .get(async (req, res) => {
-        return res.status(200).render('register', { title: 'Register' });
-    })
-    .post(async (req, res) => {
-        let validatedInput = undefined;
-        const {
-            firstNameInput,
-            lastNameInput,
-            userNameInput,
-            emailAddressInput,
-            passwordInput,
-            descriptionInput,
-            ageInput,
-        } = req.body;
+  .route("/register")
+  .get(async (req, res) => {
+    return res.status(200).render("register", { title: "Register" });
+  })
+  .post(async (req, res) => {
+    let validatedInput = undefined;
+    const {
+      firstNameInput,
+      lastNameInput,
+      userNameInput,
+      emailAddressInput,
+      passwordInput,
+      descriptionInput,
+      ageInput,
+    } = req.body;
 
-        try {
-            validatedInput = helper.createUserValidator(
-                firstNameInput,
-                lastNameInput,
-                userNameInput,
-                emailAddressInput,
-                passwordInput,
-                descriptionInput,
-                ageInput
-            );
-        } catch (e) {
-            return res
-                .status(400)
-                .render('register', { title: 'Register', error: e });
-        }
+    try {
+      validatedInput = helper.createUserValidator(
+        firstNameInput,
+        lastNameInput,
+        userNameInput,
+        emailAddressInput,
+        passwordInput,
+        descriptionInput,
+        ageInput,
+      );
+    } catch (e) {
+      return res
+        .status(400)
+        .render("register", { title: "Register", error: e });
+    }
 
-        let resDB = null;
+    let resDB = null;
 
-        try {
-            resDB = await userDataHelpers.createUser(
-                validatedInput.firstName,
-                validatedInput.lastName,
-                validatedInput.userName,
-                validatedInput.emailAddress,
-                validatedInput.password,
-                validatedInput.description,
-                validatedInput.age
-            );
-        } catch (e) {
-            const status = 400;
+    try {
+      resDB = await userDataHelpers.createUser(
+        validatedInput.firstName,
+        validatedInput.lastName,
+        validatedInput.userName,
+        validatedInput.emailAddress,
+        validatedInput.password,
+        validatedInput.description,
+        validatedInput.age,
+      );
+    } catch (e) {
+      const status = 400;
 
-            if (e === 'Internal Server Error') status = 500;
+      if (e === "Internal Server Error") status = 500;
 
-            return res
-                .status(status)
-                .render('register', { title: 'Register', error: e });
-        }
+      return res
+        .status(status)
+        .render("register", { title: "Register", error: e });
+    }
 
-        if (resDB.insertedUser) return res.redirect('/login');
+    if (resDB.insertedUser) return res.redirect("/login");
 
-        return res.render('register', { title: 'Register', error: e });
-    });
+    return res.render("register", { title: "Register", error: e });
+  });
 
 router
-    .route('/login')
-    .get(async (req, res) => {
-        return res.status(200).render('login', { title: 'Login' });
-    })
-    .post(async (req, res) => {
-        let validatedInput = undefined;
-        const { userNameInput, passwordInput } = req.body;
+  .route("/login")
+  .get(async (req, res) => {
+    return res.status(200).render("login", { title: "Login" });
+  })
+  .post(async (req, res) => {
+    let validatedInput = undefined;
+    const { userNameInput, passwordInput } = req.body;
 
-        try {
-            validatedInput = helper.loginUserValidator(
-                userNameInput,
-                passwordInput
-            );
-        } catch (e) {
-            return res
-                .status(400)
-                .render('login', { title: 'Login', error: e });
-        }
+    try {
+      validatedInput = helper.loginUserValidator(userNameInput, passwordInput);
+    } catch (e) {
+      return res.status(400).render("login", { title: "Login", error: e });
+    }
 
-        let resDB = null;
+    let resDB = null;
 
-        try {
-            resDB = await userDataHelpers.loginUser(
-                validatedInput.userName,
-                validatedInput.password
-            );
-        } catch (e) {
-            const status = 400;
+    try {
+      resDB = await userDataHelpers.loginUser(
+        validatedInput.userName,
+        validatedInput.password,
+      );
+    } catch (e) {
+      const status = 400;
 
-            if (e === 'Internal Server Error') status = 500;
+      if (e === "Internal Server Error") status = 500;
 
-            return res
-                .status(status)
-                .render('login', { title: 'Login', error: e });
-        }
+      return res.status(status).render("login", { title: "Login", error: e });
+    }
 
-        req.session.user = {
-            firstName: resDB.firstName,
-            lastName: resDB.lastName,
-            emailAddress: resDB.emailAddress,
-            userName: resDB.userName,
-            aboutMe: resDB.aboutMe,
-        };
+    req.session.user = resDB;
 
-        return res.redirect('/home');
-    });
+    return res.redirect("/home");
+  });
 
-router.route('/home').get(async (req, res) => {
-    return res
-        .status(200)
-        .render('home', { title: 'Home', userData: req.session.user });
+router.route("/home").get(async (req, res) => {
+  return res
+    .status(200)
+    .render("home", { title: "Home", userData: req.session.user });
 });
 
-router.route('/logout').get(async (req, res) => {
-    const anHourAgo = new Date();
+router.route("/logout").get(async (req, res) => {
+  const anHourAgo = new Date();
 
-    anHourAgo.setHours(anHourAgo.getHours() - 1);
+  anHourAgo.setHours(anHourAgo.getHours() - 1);
 
-    res.cookie('AuthState', '', { expires: anHourAgo });
-    res.clearCookie('AuthState');
+  res.cookie("AuthState", "", { expires: anHourAgo });
+  res.clearCookie("AuthState");
 
-    req.session.destroy();
+  req.session.destroy();
 
-    return res.redirect('/login');
+  return res.redirect("/login");
 });
 
 export default router;
--- routes/middleware.js
+++ routes/middleware.js
@@ -1,41 +1,49 @@
 // If the user posts to the server with a property called _method, rewrite the request's method
 export const rewriteUnsupportedBrowserMethods = (req, res, next) => {
-    if (req.body && req.body._method) {
-        req.method = req.body._method;
-        delete req.body._method;
-    }
+  if (req.body && req.body._method) {
+    req.method = req.body._method;
+    delete req.body._method;
+  }
 
-    next();
+  next();
 };
 
+// Authentication
 export const root = async (req, res, next) => {
-    if (req.originalUrl !== '/') return next();
+  if (req.originalUrl !== "/") return next();
 
-    if (!req.session.user) return res.redirect('/login');
+  if (!req.session.user) return res.redirect("/login");
 
-    return res.redirect('/home');
+  return res.redirect("/home");
 };
 
 export const login = async (req, res, next) => {
-    if (!req.session.user) return next();
+  if (!req.session.user) return next();
 
-    return res.redirect('/home');
+  return res.redirect("/home");
 };
 
 export const register = async (req, res, next) => {
-    if (!req.session.user) return next();
+  if (!req.session.user) return next();
 
-    return res.redirect('/home');
+  return res.redirect("/home");
 };
 
 export const home = async (req, res, next) => {
-    if (!req.session.user) return res.redirect('/login');
+  if (!req.session.user) return res.redirect("/login");
 
-    next();
+  next();
 };
 
 export const logout = (req, res, next) => {
-    if (!req.session.user) return res.redirect('/login');
+  if (!req.session.user) return res.redirect("/login");
 
-    next();
+  next();
+};
+
+// Workouts
+export const workouts = (req, res, next) => {
+  if (!req.session.user) return res.redirect("/login");
+
+  next();
 };
