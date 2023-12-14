ffbf291f0e4fd36d828e68dc85ccee03da5d816f
Arnavpmr <arnavmar16@gmail.com>
2023-12-14 14:26:32 -0500

    Implemented route templates for challenges

--- routes/challenges.js
+++ routes/challenges.js
@@ -1,25 +1,30 @@
-import usrFuncs from "../data/challenges.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.route("/").get(async (req, res) => {
-  // const challenges = await usrFuncs.getAllChallenges();
-  // const curDate = new Date();
-
-  // const curChallenges = challenges.filter(
-  //     (challenge) => challenge.deadline > curDate
-  // );
-
-  // const pastChallenges = challenges.filter(
-  //     (challenge) => challenge.deadline < curDate
-  // );
+  //TODO: Implement fetching of data for current and past challenges by retrieving it from some mongo object
+  const curChallenge = [];
+  const pastChallenges = [];
 
   return res.status(200).render("challenges", {
-    // curChallenges: curChallenges,
-    // pastChallenges: pastChallenges,
+    curChallenge: curChallenge,
+    pastChallenges: pastChallenges,
     user: req.session.user,
   });
 });
+
+router.route("/challenge/submit").post(async (req, res) => {});
+
+router.route("/challenge/submissions").get(async (req, res) => {});
+
+router.route("/challenge/:submissionId").post(async (req, res) => {});
+
+router.route("/challenge").post(async (req, res) => {});
+
+router
+  .route("/challenge/:challengeId")
+  .put(async (req, res) => {})
+  .delete(async (req, res) => {});
 
 export default router;
