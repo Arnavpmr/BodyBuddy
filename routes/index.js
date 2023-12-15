5d0e284f21d4f9c355dea58d6feae22e38864eea
Arnavpmr <arnavmar16@gmail.com>
2023-12-15 14:13:56 -0500

    Fixed issue with routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import authRoutes from "./auth.js";
 import challengesRoutes from "./challenges.js";
 import aboutRoute from "./about.js";
 import homeRoute from "./home.js";
-import usersRoutes from "./users.js";
+import profileRoutes from "./profile.js";
 import workoutsRoutes from "./workouts.js";
 
 const constructorMethod = (app) => {
@@ -11,7 +11,7 @@ const constructorMethod = (app) => {
   app.use("/about", aboutRoute);
   app.use("/home", homeRoute);
   app.use("/challenges", challengesRoutes);
-  app.use("/user", usersRoutes);
+  app.use("/user", profileRoutes);
   app.use("/workouts", workoutsRoutes);
 
   app.use("*", (req, res) => {
