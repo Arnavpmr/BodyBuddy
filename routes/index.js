fb35aac3bedb10e0514ab92b5bfdf8c6a1c51c7a
AidanGio <agiordan@stevens.edu>
2023-12-18 23:00:12 -0500

    html fix

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ const constructorMethod = (app) => {
   app.use("*", (req, res) => {
     return res.status(404).render("error", {
       error: "The page you requested does not exist",
+      title: "Error",
     });
   });
 };
