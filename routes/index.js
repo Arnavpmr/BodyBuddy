f8d3d29376b5782beb330c0ca9ba7dd8bda8d59a
AidanGio <agiordan@stevens.edu>
2023-12-18 22:58:54 -0500

    placeholder for the profile page

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,6 @@ const constructorMethod = (app) => {
   app.use("*", (req, res) => {
     return res.status(404).render("error", {
       error: "The page you requested does not exist",
-      title: "Error",
     });
   });
 };
